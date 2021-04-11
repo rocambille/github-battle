b72435f7683f63dfbf29f0579b5711581ad2821e
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2021-04-11 10:31:28 +0200

    added index of pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import About from './About';
+import Home from './Home';
+
+const pages = [
+  {
+    id: 0,
+    name: 'Home',
+    path: '/home',
+    Component: Home,
+  },
+  {
+    id: 1,
+    name: 'About',
+    path: '/about',
+    Component: About,
+  },
+];
+
+export default pages;
