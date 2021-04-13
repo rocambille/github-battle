eedf1a3e6abc43469f595c640178268cb151d312
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2021-04-13 15:04:24 +0200

    fetched GitHub user count in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../components/Card';
 
 function Home() {
+  const [totalCount, setTotalCount] = useState();
+
+  useEffect(() => {
+    fetch('https://api.github.com/search/users?q=type%3Auser')
+      .then((response) => response.json())
+      .then((data) => {
+        setTotalCount(data.total_count);
+      });
+  }, []);
+
   return (
     <>
+      <p>
+        Start a battle between our
+        {' '}
+        {totalCount}
+        {' '}
+        champions
+      </p>
       <Card />
       <Card />
     </>
